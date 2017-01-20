273a9b41ba03ad1eb0dce3bbd91d3791a0b0d936
Jimi6 <1315576808@qq.com>
2017-01-20 19:14:58 +0800

    v1.0

---
+++ CssTask/CSS07/js/slide-left.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+	(function(){
+		var _btn  = document.querySelector(".btn-slide-bar"),
+			_body = document.querySelector("body");
+			_btn.onclick = function(){
+				_body.classList.toggle("active");
+			}
+	})(window)
+},false);
